1080a2643f67c2ffd6db20994d64507f7d0ecf00
julelies <julia.eder1999@gmail.com>
2019-12-17 16:47:06 +0100

    commit add number project

---
+++ JEder/MyGame/Assets/Scripts/Control.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Control : MonoBehaviour
+{
+    public InputField input1;
+    public InputField input2;
+    public Text result;
+
+    float varA;
+    float varB;
+
+    public void SetResult()
+    {
+        result.text = CalculateNumbers();
+    }
+
+    string CalculateNumbers()
+    {
+        if (string.IsNullOrEmpty(input1.text) || string.IsNullOrEmpty(input2.text))
+        {
+            string outputError = "ERROR";
+            return outputError;
+        }
+        else
+        {
+            varA = float.Parse(input1.text);
+            varB = float.Parse(input2.text);
+
+            string outputSuccess = (varA + varB).ToString();
+            return outputSuccess;
+        }
+    }
+
+    public void ResetApp()
+    {
+        input1.text = "";
+        input2.text = "";
+        result.text = "";
+    }
+}
